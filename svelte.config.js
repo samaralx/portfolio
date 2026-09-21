8ee9aef77918069651ce5afe029bbb948bc3bde6
pinglu85 <luping8576@hotmail.com>
2026-09-21 22:12:44 +0200

    allow css unsafe-inline

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,21 @@
 import adapter from "@sveltejs/adapter-static";
 
 export default {
-	kit: {
-		adapter: adapter(),
-		csp: {
-			directives: {
-				"script-src": ["self"],
-				"base-uri": ["self"],
-				"form-action": ["self"],
-				"default-src": ["self"],
-			},
-			// must be specified with either the `report-uri` or `report-to` directives, or both
-			reportOnly: {
-				"script-src": ["self"],
-				"report-uri": ["/"],
-			},
-		},
-	},
+  kit: {
+    adapter: adapter(),
+    csp: {
+      directives: {
+        "script-src": ["self"],
+        "base-uri": ["self"],
+        "form-action": ["self"],
+        "default-src": ["self"],
+        "style-src": ["self", "unsafe-inline"],
+      },
+      // must be specified with either the `report-uri` or `report-to` directives, or both
+      reportOnly: {
+        "script-src": ["self"],
+        "report-uri": ["/"],
+      },
+    },
+  },
 };
